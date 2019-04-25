cf9e5f2565c5b535c580a8991e5fe56a9e6ff5aa
SimonDanisch <sdanisch@gmail.com>
2019-04-25 13:57:27 +0200

    use tables

--- src/api.jl
+++ src/api.jl
@@ -11,6 +11,7 @@ struct InstancedProgram
     per_instance::AbstractArray
 end
 
+name_type_iter(x) = (s = Tables.schema(x); zip(s.names, s.types))
 
 function InstancedProgram(
         context::AbstractContext, shader,
@@ -19,27 +20,32 @@ function InstancedProgram(
         uniforms...
     )
 
-    instance = convert_uniform(context, instance)
-    Vertex = eltype(instance)
-    per_instance = convert_uniform(context, per_instance)
-    Instance = eltype(per_instance)
-    uniforms = Dict{Symbol, Any}()
+    # instance = convert_uniform(context, instance)
+    # per_instance = convert_uniform(context, per_instance)
+    c_uniforms = Dict{Symbol, Any}()
     src = sprint() do io
-        for name in (fieldnames(Vertex)..., fieldnames(Instance)...,)
-            T = fieldtype(Vertex, name)
+        println(io, "// Instance inputs: ")
+        for (name, T) in name_type_iter(instance)
             println(io, "in ", type_string(context, T), " $name;")
         end
+
+        println(io, "\n// Per instance attributes: ")
+        for (name, T) in name_type_iter(per_instance)
+            println(io, "in ", type_string(context, T), " $name;")
+        end
+
+        println(io, "\n// Uniforms: ")
         for (k, v) in uniforms
             vc = convert_uniform(context, v)
             T = typeof(vc)
             println(io, "uniform ", type_string(context, T), " $name;")
-            uniforms[k] = vc
+            c_uniforms[k] = vc
         end
         println(io)
         println(io, shader)
     end
     InstancedProgram(
-        Program(context, uniforms, src, instance),
+        Program(context, instance, c_uniforms, src),
         per_instance
     )
 end
