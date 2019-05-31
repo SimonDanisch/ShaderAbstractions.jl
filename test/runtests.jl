16620b4c94bb9d57be4fa2f3ddb150c910843c0a
SimonDanisch <sdanisch@gmail.com>
2019-05-31 12:06:21 +0200

    add readme

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using ShaderAbstractions: VertexArray
 using Test, GeometryTypes
 import GeometryBasics
 
-struct Bla <: ShaderAbstractions.AbstractContext end
+struct WebGL <: ShaderAbstractions.AbstractContext end
 
 m = GLNormalMesh(Sphere(Point3f0(0), 1f0))
 
@@ -11,14 +11,13 @@ mvao = VertexArray(m)
 instances = VertexArray(positions = rand(GeometryBasics.Point{3, Float32}, 100))
 
 x = ShaderAbstractions.InstancedProgram(
-    Bla(),
+    WebGL(),
     "void main(){}\n", "void main(){}\n",
     mvao,
     instances,
-    model = GeometryTypes.Mat4f0(I),
-    view = GeometryTypes.Mat4f0(I),
-    projection = GeometryTypes.Mat4f0(I),
-
+    model = Mat4f0(I),
+    view = Mat4f0(I),
+    projection = Mat4f0(I),
 )
 
 @test x.program.fragment_source == read(joinpath(@__DIR__, "test.frag"), String)
